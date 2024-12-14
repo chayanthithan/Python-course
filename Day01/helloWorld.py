ae14d00ca520c341ba0bfe7afcc134e16bfae9c2
cchayanthithan <kumarchayan31@gmail.com>
2024-12-13 22:40:25 -0600

    new series

---
+++ Day01/helloWorld.py
@@ -0,0 +1,2 @@
+# hello  this is my first progeram 2024-12-13
+print("hello world! i am ready to give my best")
---
+++ Day01/variables.py
@@ -0,0 +1,19 @@
+# string
+fname="chayan kumar"
+print(f"full name is {fname}")
+
+#Integer
+age =25
+print(f"i am {age} years old")
+
+#float
+gpa =3.2
+print(f"my gpa is:{gpa}")
+
+#boolean
+is_student = True
+
+if is_student:
+    print(f"you are a student {is_student}")
+else:
+    print(f"you are not a student {is_student}")
